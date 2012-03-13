483d8053ae2425ac6ad4706f05bb29483710c8ae
Ollie Armstrong <sexii.ollie@gmail.com>
2012-03-13 16:34:28 +0000

    Prep for release

--- cdn-sync-tool.php
+++ cdn-sync-tool.php
@@ -5,16 +5,17 @@ Plugin URI: http://www.olliearmstrong.co.uk
 Description: Allows wordpress owners to sync their static files to CDN
 Author: Fubra Limited
 Author URI: http://www.catn.com
-Version: 3.0
+Version: 2.0
 */
 
 global $wpdb;
 
 define('CST_DIR', dirname(__FILE__).'/');
-define('CST_VERSION', '1.0');
+define('CST_VERSION', '2.0');
 define('CST_URL', admin_url('options-general.php'));
 define('CST_FILE', __FILE__);
 define('CST_TABLE_FILES', $wpdb->get_blog_prefix().'cst_files');
+define('CST_CONTACT_EMAIL', 'support@catn.com');
 
 
 if (is_admin()) {
