8444edd378bfe27bd991a8d7e5252280e94963cb
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-08-08 16:58:04 +0100

    Doc

--- lib/Site.php
+++ lib/Site.php
@@ -29,20 +29,28 @@ class Cst_Site {
 	public function combineFiles($buffer) {
 		$stylesheetCombined = '';
 		$stylesheets = array();
+		// Find all stylesheet links
 		preg_match_all('$<link.*rel=[\'"]stylesheet[\'"].*?>$', $buffer, $stylesheets);
 		foreach ($stylesheets[0] as $stylesheet) {
+			// Remove the stylesheet link from $buffer
 			$buffer = str_replace($stylesheet, '', $buffer);
+
+			// Get the filepath of the stylesheet
 			preg_match('$href=[\'"]'.get_bloginfo('wpurl').'(.*?)\??[\'"]$', $stylesheet, $href);
 			$path = $href[1];
 			$path = preg_replace('$\.css(\?.*)$', '.css', $path);
 			$path = ABSPATH.ltrim($path, '/');
+
 			$file = file_get_contents($path);
 
-			// TODO: go through stylesheet and replace all urls with absolute paths
+			// TODO:
+			// * go through stylesheet and replace all urls with absolute paths
+			// * check file exclusion list
 
 			$stylesheetCombined .= $file;
 		}
 
+		// Create unique filename based on the md5 of the content
 		$hash = md5($stylesheetCombined);
 		$combinedFilename = ABSPATH.get_option('cst-css-savepath').'/'.$hash.'.css';
 
@@ -56,6 +64,7 @@ class Cst_Site {
 			$linkTag = '<link rel="stylesheet" type="text/css" href="'.$fileUrl.'" />';
 			$buffer = preg_replace('$<head[^er]*>$', '<head>'.$linkTag, $buffer);
 		}
+
 		return $buffer;
 	}
 }
