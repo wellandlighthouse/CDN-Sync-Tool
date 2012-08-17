818d04d873a633e3454ae6c17ea4d5b62e755b27
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-08-17 15:00:19 +0100

    Add required files for test framework

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+// Load the test environment
+// https://github.com/nb/wordpress-tests
+
+$path = '/home/ollie/src/wordpress-tests/bootstrap.php';
+
+if (file_exists($path)) {
+	$GLOBALS['wp_tests_options'] = array(
+		'active_plugins' => array('cdn-sync-tool/cdn-sync-tool.php')
+	);
+	require_once $path;
+} else {
+	exit("Couldn't find wordpress-tests/bootstrap.php\n");
+}
