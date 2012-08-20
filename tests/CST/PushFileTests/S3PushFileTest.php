0d270ddcfb87cb130578878319c3a9ca483d4198
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-08-20 17:35:44 +0100

    Added image upload test

--- tests/CST/PushFileTests/S3PushFileTest.php
+++ tests/CST/PushFileTests/S3PushFileTest.php
@@ -6,7 +6,14 @@ class S3PushFileTest extends WP_UnitTestCase {
 		$this->cst = $GLOBALS['core'];
 	}
 
-	public function testS3Upload() {
-		$this->assertTrue(true);
+	public function testS3UploadImage() {
+		require CST_DIR.'tests/CST/ConnectionTests/S3ConnectionSettings.php';
+		update_option('cst-s3-accesskey', $accesskey);
+		update_option('cst-s3-secretkey', $secretkey);
+		update_option('cst-s3-bucket', $bucket);
+		$this->cst->connectionType = 'S3';
+		$this->cst->createConnection();
+
+		$this->cst->pushFile(CST_DIR.'tests/CST/PushFileTests/300x120.gif', 'image.gif');
 	}
 }
