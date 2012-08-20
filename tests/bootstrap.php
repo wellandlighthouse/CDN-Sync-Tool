c9168ac0038f36922fbf8b0abe659a035c115a69
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-08-20 15:07:08 +0100

    More travis config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,8 +3,15 @@
 // https://github.com/nb/wordpress-tests
 
 $path = '/home/ollie/src/wordpress-tests/bootstrap.php';
+$travispath = '~/src/wordpress-tests/bootstrap.php';
 
 if (file_exists($path)) {
+	$GLOBALS['wp_tests_options'] = array(
+		'active_plugins' => array('cdn-sync-tool/cdn-sync-tool.php'),
+		'cst-unit-test' => true
+	);
+	require_once $path;
+} else if (file_exists($travispath)) {
 	$GLOBALS['wp_tests_options'] = array(
 		'active_plugins' => array('cdn-sync-tool/cdn-sync-tool.php'),
 		'cst-unit-test' => true
