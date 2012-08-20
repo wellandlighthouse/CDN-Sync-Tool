f7c6b3dfba7c6c2e6aa783204e2f34babe010d22
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-08-20 15:27:31 +0100

    Bootstrap path more general

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,7 @@
 // Load the test environment
 // https://github.com/nb/wordpress-tests
 
-$path = '/home/ollie/src/wordpress-tests/bootstrap.php';
-$travispath = '~/src/wordpress-tests/bootstrap.php';
+$path = '/home/'.getenv('USER').'/src/wordpress-tests/bootstrap.php';
 
 if (file_exists($path)) {
 	$GLOBALS['wp_tests_options'] = array(
@@ -11,12 +10,6 @@ if (file_exists($path)) {
 		'cst-unit-test' => true
 	);
 	require_once $path;
-} else if (file_exists($travispath)) {
-	$GLOBALS['wp_tests_options'] = array(
-		'active_plugins' => array('cdn-sync-tool/cdn-sync-tool.php'),
-		'cst-unit-test' => true
-	);
-	require_once $path;
 } else {
-	exit("Couldn't find wordpress-tests/bootstrap.php\n");
+	exit("Couldn't find wordpress-tests/bootstrap.php at $path \n");
 }
