5ee6f629aef1a29e29654fd86a7b42fedd273668
Ollie Armstrong <me@olliearmstrong.co.uk>
2012-08-20 17:10:29 +0100

    Added barebones push file tests

---
+++ tests/CST/PushFileTests/S3PushFileTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class S3PushFileTest extends WP_UnitTestCase {
+	public function setUp() {
+		parent::setUp();
+		$this->cst = $GLOBALS['core'];
+	}
+
+	public function testS3Upload() {
+		$this->assertTrue(true);
+	}
+}
